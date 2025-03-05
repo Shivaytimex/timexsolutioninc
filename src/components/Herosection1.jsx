30f473cec389924af106fc3420782eb633469a35
mian kashif <miankashifmehmood@gmail.com>
2025-03-05 20:10:45 +0500

    changes 3/5/25

--- src/components/Herosection1.jsx
+++ src/components/Herosection1.jsx
@@ -1,35 +1,35 @@
-import { Link } from "react-router";
-import TypewriterEffect from "./TypewriterEffect";
-import Magnet from "../utils/Magnet";
-import { Stars } from "./Stars";
-import { motion } from "framer-motion";
-import { AniButton } from "../utils/ButtonAnimation";
+import { Link } from 'react-router'
+import TypewriterEffect from './TypewriterEffect'
+import Magnet from '../utils/Magnet'
+import { Stars } from './Stars'
+import { motion } from 'framer-motion'
+import { AniButton } from '../utils/ButtonAnimation'
 
 // optimized code
 function HeroSection1() {
   return (
-    <div className="relative bg-gradient-to-b from-black from-10% via-primary via-100% to-black to-90% min-h-screen overflow-hidden py-8 md:py-10 mt-24 lg:mt-28  xl:mt-28">
-      <section className="bg-gradient-to-b from-transparent via-PurpleDark/10 to-transparent mx-auto px-4 relative">
+    <div className='relative bg-gradient-to-b from-black from-10% via-primary via-100% to-black to-90% min-h-screen overflow-hidden py-8 md:py-10 mt-24 lg:mt-28  xl:mt-28'>
+      <section className='bg-gradient-to-b from-transparent via-PurpleDark/10 to-transparent mx-auto px-4 relative'>
         <Stars />
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 relative">
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-8 relative'>
           {/* First Column: Heading Text */}
-          <div className="text-center relative lg:mt-12">
+          <div className='text-center relative lg:mt-12'>
             <motion.h1
-              className="lg:text-5xl text-white text-3xl font-bold leading-tight mb-4 text-center"
+              className='lg:text-5xl text-white text-3xl font-bold leading-tight mb-4 text-center'
               initial={{ opacity: 0, y: -20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.8 }}
             >
-              <div className="text-transparent bg-clip-text bg-white/90">
+              <div className='text-transparent bg-clip-text bg-white/90'>
                 Connecting <br />
                 your brand to the
                 <br />
-                <span className=""> world</span>
+                <span className=''> world</span>
               </div>
               <TypewriterEffect />
             </motion.h1>
             <motion.p
-              className="text-lg text-transparent bg-clip-text bg-white mb-1"
+              className='text-lg text-transparent bg-clip-text bg-white mb-1'
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.8, delay: 0.2 }}
@@ -42,29 +42,29 @@ function HeroSection1() {
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.8, delay: 0.4 }}
             >
-              <Link to={"/contact"}>
+              <Link to={'/contact'}>
                 <AniButton
-                  text="Get Started"
-                  buttonClass="bg-primary text-white font-semibold px-10 py-5 rounded-full border-2 border-white"
-                  textClass="bg-white text-primary text-3xl"
+                  text='Get Started'
+                  buttonClass='bg-primary text-white font-semibold px-10 py-5 rounded-full border-2 border-white'
+                  textClass='bg-white text-primary text-3xl'
                 />
               </Link>
             </motion.div>
           </div>
 
           {/* Second Column: Image */}
-          <div className="w-full h-full relative ">
+          <div className='w-full h-full relative '>
             <motion.div
-              className="h-auto lg:w-[470px] lg:-mt-2 mx-auto object-cover"
+              className='h-auto lg:w-[470px] lg:-mt-2 mx-auto object-cover'
               initial={{ opacity: 0, scale: 0.8 }}
               animate={{ opacity: 1, scale: 1 }}
               transition={{ duration: 0.8, delay: 0.6 }}
             >
               <Magnet padding={50} disabled={false} magnetStrength={50}>
                 <img
-                  src="/Web-Banner-1.webp"
-                  alt="Web-Banner"
-                  className="rounded-lg object-cover"
+                  src='/Web-Banner-1.webp'
+                  alt='Web-Banner'
+                  className='rounded-lg object-cover'
                 />
               </Magnet>
             </motion.div>
@@ -72,7 +72,7 @@ function HeroSection1() {
         </div>
       </section>
     </div>
-  );
+  )
 }
 
-export default HeroSection1;
+export default HeroSection1
--- src/components/TypewriterEffect.jsx
+++ src/components/TypewriterEffect.jsx
@@ -1,58 +1,59 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useState, useEffect } from "react";
+"use client"
+import { useState, useEffect } from 'react'
 
 const Typewriter = () => {
   const texts = [
-    "PPC Campaigns",
-    "Google Ads",
-    "Web Development",
-    "Digital Services",
-  ];
-  const [currentText, setCurrentText] = useState("");
-  const [textIndex, setTextIndex] = useState(0);
-  const [isDeleting, setIsDeleting] = useState(false);
+    'PPC Campaigns',
+    'Google Ads',
+    'Web Development',
+    'Digital Services',
+  ]
+  const [currentText, setCurrentText] = useState('')
+  const [textIndex, setTextIndex] = useState(0)
+  const [isDeleting, setIsDeleting] = useState(false)
 
   useEffect(() => {
-    const typingSpeed = 150; // Speed of typing
-    const deletingSpeed = 100; // Speed of deleting
-    const switchDelay = 2000; // Delay before switching text
+    const typingSpeed = 150 // Speed of typing
+    const deletingSpeed = 100 // Speed of deleting
+    const switchDelay = 2000 // Delay before switching text
 
-    let timer;
+    let timer
 
     if (isDeleting) {
       // Delete text
       timer = setTimeout(() => {
-        setCurrentText((prev) => prev.slice(0, prev.length - 1));
-      }, deletingSpeed);
+        setCurrentText((prev) => prev.slice(0, prev.length - 1))
+      }, deletingSpeed)
     } else {
       // Type text
       timer = setTimeout(() => {
-        setCurrentText((prev) => texts[textIndex].slice(0, prev.length + 1));
-      }, typingSpeed);
+        setCurrentText((prev) => texts[textIndex].slice(0, prev.length + 1))
+      }, typingSpeed)
     }
 
     // If current text is fully typed, start deleting after some time
     if (currentText === texts[textIndex] && !isDeleting) {
-      setTimeout(() => setIsDeleting(true), switchDelay);
-    } else if (currentText === "" && isDeleting) {
-      setIsDeleting(false);
-      setTextIndex((prevIndex) => (prevIndex + 1) % texts.length); // Loop through texts
+      setTimeout(() => setIsDeleting(true), switchDelay)
+    } else if (currentText === '' && isDeleting) {
+      setIsDeleting(false)
+      setTextIndex((prevIndex) => (prevIndex + 1) % texts.length) // Loop through texts
     }
 
-    return () => clearTimeout(timer); // Clean up the timer on component unmount
-  }, [currentText, isDeleting, textIndex, texts]);
+    return () => clearTimeout(timer) // Clean up the timer on component unmount
+  }, [currentText, isDeleting, textIndex, texts])
 
   return (
     <>
-      <span className="text-transparent bg-clip-text bg-gradient-to-r from-PurpleLight to-PurpleDark">
+      <span className='text-transparent bg-clip-text bg-gradient-to-r from-PurpleLight to-PurpleDark'>
         {currentText}
       </span>
-      <span className="animate-pulse text-transparent bg-clip-text bg-gradient-to-r from-PurpleLight to-PurpleDark">
+      <span className='animate-pulse text-transparent bg-clip-text bg-gradient-to-r from-PurpleLight to-PurpleDark'>
         |
-      </span>{" "}
+      </span>{' '}
       {/* Blinking cursor */}
     </>
-  );
-};
+  )
+}
 
-export default Typewriter;
+export default Typewriter
